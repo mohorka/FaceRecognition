b42b9241d843ee35e940c74f904f89afb0649f3f
mohorka <victoria2000dzuba@gmail.com>
2022-03-11 22:26:04 +0300

    impl methods from task 1

---
+++ ViolaJones.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Combobox
+
+import cv2 as cv
+from PIL import Image, ImageTk
+
+
+def download_photo():
+    global image_path
+    image_path = filedialog.askopenfilename()
+
+
+def ViolaJones():
+    image = cv.imread(image_path)
+    grayscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    face_cascade = cv.CascadeClassifier(
+        cv.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+    eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_eye.xml")
+    detected_faces = face_cascade.detectMultiScale(grayscale_image)
+    detected_eyes = eye_cascade.detectMultiScale(grayscale_image)
+    for (column, row, width, height) in detected_faces:
+        cv.rectangle(
+            image, (column, row), (column + width, row + height), (255, 0, 0), 4
+        )
+    for (column, row, width, height) in detected_eyes:
+        cv.rectangle(
+            image, (column, row), (column + width, row + height), (0, 255, 0), 4
+        )
+
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    image = Image.fromarray(image)
+    width, height = image.size
+    image = image.resize((width // 3, height // 3), Image.ANTIALIAS)
+    Label(window, text="Result").grid(row=1, column=3)
+    photo = ImageTk.PhotoImage(image)
+    label = Label(window, image=photo)
+    label.image = photo
+    label.grid(row=2, column=3)
+
+
+window = Tk()
+window.title("Viola Jones Detector")
+window.geometry("800x400")
+
+button = Button(window, text="Download photo", command=download_photo, width=20)
+button.grid(column=0, row=0)
+
+button = Button(window, text="Go", command=ViolaJones, width=20)
+button.grid(column=0, row=1)
+window.mainloop()
---
+++ templateMatching.py
@@ -0,0 +1,121 @@
+from random import randint
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Combobox
+
+import cv2 as cv
+from PIL import Image, ImageTk
+from sklearn.datasets import fetch_olivetti_faces
+
+
+def download_photo():
+    global image_path
+    global is_randomly
+    is_randomly = 0
+    image_path = filedialog.askopenfilename()
+
+
+def download_template():
+    global template_path
+    template_path = filedialog.askopenfilename()
+
+
+def get_from_dataset():
+    global is_randomly
+    global rand_image
+    global rand_template
+    is_randomly = 1
+    faces = fetch_olivetti_faces()
+    faces = faces.images
+    img_idx = randint(0, len(faces) - 1)
+    temp_idx = randint(0, len(faces) - 1)
+    rand_image = faces[img_idx] * 255
+    rand_template = faces[temp_idx] * 255
+    rand_template = rand_template[10:50, 10:50]
+
+
+def matchingTemplateDetector():
+    m = method_name.get()
+    if is_randomly == 1:
+        image = rand_image.copy()
+        template = rand_template.copy()
+    else:
+        image = cv.imread(image_path, 0)
+        template = cv.imread(template_path, 0)
+    method = eval("cv." + m)
+    w, h = template.shape[::-1]
+
+    res = cv.matchTemplate(image, template, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    image = cv.rectangle(image, top_left, bottom_right, 255, 2)
+
+    image = Image.fromarray(image)
+    template = Image.fromarray(template)
+
+    width, height = image.size
+    widthtemp, heighttemp = template.size
+
+    image = image.resize((width // 3, height // 3), Image.ANTIALIAS)
+    template = template.resize((widthtemp // 3, heighttemp // 3), Image.ANTIALIAS)
+
+    if is_randomly == 1:
+        image = image.resize((200, 200), Image.ANTIALIAS)
+        template = template.resize((200, 200), Image.ANTIALIAS)
+
+    Label(window, text="Result").grid(row=3, column=1)
+    Label(window, text="Template").grid(row=3, column=3)
+
+    photo = ImageTk.PhotoImage(image)
+    label = Label(window, image=photo)
+    label.image = photo
+    label.grid(row=4, column=1)
+
+    temp_photo = ImageTk.PhotoImage(template)
+    label2 = Label(window, image=temp_photo)
+    label2.image = temp_photo
+    label2.grid(row=4, column=3)
+
+
+window = Tk()
+window.title("Template Matching Detector")
+window.geometry("1200x600")
+
+lbl = Label(window, text="Choose method")
+lbl.grid(column=0, row=0)
+
+method_name = StringVar()
+
+
+combo = Combobox(window, textvariable=method_name)
+combo["values"] = (
+    "TM_CCOEFF",
+    "TM_CCOEFF_NORMED",
+    "TM_CCORR",
+    "TM_CCORR_NORMED",
+    "TM_SQDIFF",
+    "TM_SQDIFF_NORMED",
+)
+combo.current(0)
+combo.grid(column=0, row=1)
+
+button1 = Button(window, text="Download photo", command=download_photo, width=20)
+button1.grid(column=1, row=1)
+
+button2 = Button(window, text="Download template", command=download_template, width=20)
+button2.grid(column=2, row=1)
+
+button3 = Button(window, text="Choose randomly", command=get_from_dataset, width=20)
+button3.grid(column=3, row=1)
+
+button = Button(window, text="Go", command=matchingTemplateDetector, width=20)
+button.grid(column=0, row=2)
+
+
+window.mainloop()
